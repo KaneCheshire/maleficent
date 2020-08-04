e30086530498f85465c1c32ca1b26d80d3ea98fb
Kane Cheshire <kane.cheshire@googlemail.com>
2020-08-04 18:49:55 +0100

    Tidy up default Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,28 +1,12 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Maleficent",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Maleficent",
-            targets: ["Maleficent"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    products: [.library(name: "Maleficent", targets: ["Maleficent"]),],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Maleficent",
-            dependencies: []),
-        .testTarget(
-            name: "MaleficentTests",
-            dependencies: ["Maleficent"]),
+        .target(name: "Maleficent"),
+        .testTarget(name: "MaleficentTests", dependencies: ["Maleficent"]),
     ]
 )
