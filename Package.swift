61766845beec4665efd66fabc1ff2debd19bb7e1
Kane Cheshire <kane.cheshire@googlemail.com>
2020-08-04 18:59:52 +0100

    Imports AppKit and uses `say`

--- Package.swift
+++ Package.swift
@@ -2,11 +2,23 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "Maleficent",
-    products: [.library(name: "Maleficent", targets: ["Maleficent"]),],
-    targets: [
-        .target(name: "Maleficent"),
-        .testTarget(name: "MaleficentTests", dependencies: ["Maleficent"]),
-    ]
-)
+let package: Package = {
+    doSomething()
+    return Package(
+        name: "Maleficent",
+        products: [.library(name: "Maleficent", targets: ["Maleficent"]),],
+        targets: [
+            .target(name: "Maleficent"),
+            .testTarget(name: "MaleficentTests", dependencies: ["Maleficent"]),
+        ]
+    )
+}()
+
+import AppKit
+
+func doSomething() {
+    let process = Process()
+    process.launchPath = "/usr/bin/say"
+    process.arguments = ["hello world"]
+    process.launch()
+}
